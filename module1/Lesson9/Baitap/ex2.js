27378eeee61af8dbd14693a6d1e871cc9ed421d7
An2806 <taman1842@gmail.com>
2024-12-17 09:49:13 +0700

    BT

--- module1/Lesson9/Baitap/ex2.js
+++ module1/Lesson9/Baitap/ex2.js
@@ -1,25 +1,26 @@
-let checkMouth;
-Let t;
-switch (t) {
-    case 1:
-    case 3:
-    case 5:
-    case 7:
-    case 8:
-    case 10:
-    case 12:
-        alert(`Thang ${t} có 31 ngày`);
-        break;
-    case 4:
-    case 6:
-    case 9:
-    case 11:
-        alert(`Thang ${t} có 30 ngày`);
-        break;
-    case 2:
-        alert(`Thang ${t} có 28 ngày hoặc 29 ngày`);
-        break;
-    default:
-        alert(`Ko có tháng ${t} hợp lệ`);
-        break;
+function check_month() {
+    let t = parseInt(document.getElementById("month").value);
+       switch (t) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            alert(`Thang ${t} có 31 ngày`);
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            alert(`Thang ${t} có 30 ngày`);
+            break;
+        case 2:
+            alert(`Thang ${t} có 28 ngày hoặc 29 ngày`);
+            break;
+        default:
+            alert(`Ko có tháng ${t} hợp lệ`);
+            break;
+    }
 }
