5ff1a25d503ad98d4fe3de5eab448378fb08678f
An2806 <taman1842@gmail.com>
2024-12-17 11:06:42 +0700

    TH

--- module1/Lesson 10/TH/ex2.js
+++ module1/Lesson 10/TH/ex2.js
@@ -1,7 +1,8 @@
-let num = prompt("Enter a number:");
-let totarl = 0;
-let i = 1
-for(;i < num;i+=1) {
-    totarl = totarl + i;
-    alert(totarl);
+let num=+prompt("Enter a number:");
+let total = 0;
+let i = 1;
+for (; i <= num; i+=1) {
+    total = total + i;
+    alert(total);
 }
+
