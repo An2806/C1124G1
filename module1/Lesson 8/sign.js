c0808a1559efcd1c4d0cf5b9d8729b61831f7c3b
An2806 <taman1842@gmail.com>
2024-12-17 14:37:25 +0700

    TH

--- module1/Lesson 8/sign.js
+++ module1/Lesson 8/sign.js
@@ -1,20 +1,24 @@
-let value =+prompt('Type a number', 0);
-if (value > 0) {
-    alert(1);
-} else if (value < 0) {
-    alert(-1);
-} else {
-    alert(0);
-}
-switch(value) {
-    case 0:
-        alert(0);
-        break;
-    case 1:
-        alert(1);
-        break;
-    case 2:
-        alert(-1);
-        break;
-}
+// let value =+prompt('Type a number', 0);
+// if (value > 0) {
+//     alert(1);
+// } else if (value < 0) {
+//     alert(-1);
+// } else {
+//     alert(0);
+// }
+// switch(value) {
+//     case 0:
+//         alert(0);
+//         break;
+//     case 1:
+//         alert(1);
+//         break;
+//     case 2:
+//         alert(-1);
+//         break;
+// }
 
+let count = 0;
+while (count++ < 10) {
+    count++;
+}
